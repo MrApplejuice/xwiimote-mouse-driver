369e9912ea7770a94a458839fc90f3727df192c1
Paul Konstantin Gerke <paulkgerke@craftware.info>
2024-01-17 22:55:10 +0100

    Add wiimote image

---
+++ src/driver/keymap/img-rle64.py
@@ -0,0 +1,88 @@
+"""
+This file is part of xwiimote-mouse-driver.
+
+Foobar is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+
+Foobar is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+Foobar. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import sys
+import argparse
+from PIL import Image
+import numpy as np
+import struct
+import lzma
+import base64
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert images to rle64 format.")
+
+    parser.add_argument("image", type=str, help="Image to convert.")
+    parser.add_argument("--length", type=int, default=None, help="Maximum line length.")
+    parser.add_argument(
+        "--pyraw", action="store_true", help="Output python raw strings code."
+    )
+
+    args = parser.parse_args()
+
+    img = Image.open(args.image)
+    imgdata = np.asarray(img)
+    height, width, ncolors = imgdata.shape
+
+    rle_data = []
+    backrefs = []
+    last_sample = None
+    for y in range(height):
+        for x in range(width):
+            sample = imgdata[y][x].tolist()
+            if sample == last_sample:
+                rle_data[-1][0] += 1
+            elif sample in backrefs:
+                rle_data.append([1, backrefs.index(sample)])
+                last_sample = sample
+            else:
+                rle_data.append([1, sample])
+                backrefs.append(sample)
+                last_sample = sample
+
+    if len(backrefs) > 0xFFFF:
+        raise Exception("Too many backrefs.")
+
+    binary_rle_data = struct.pack("<HHH", width, height, ncolors)
+    for reps, sample in rle_data:
+        if isinstance(sample, int):
+            binary_rle_data += struct.pack("<lH", -reps, sample)
+        else:
+            binary_rle_data += struct.pack("<l" + "B" * ncolors, reps, *sample)
+
+    lzma_data = lzma.compress(binary_rle_data, preset=9)
+    b85data = base64.b85encode(lzma_data).decode("ascii")
+    total_size = len(b85data)
+
+    if args.length is not None:
+        b85data = [
+            b85data[i : i + args.length] for i in range(0, len(b85data), args.length)
+        ]
+    else:
+        b85data = [b85data]
+
+    for line in b85data:
+        if args.pyraw:
+            line = f'r"{line}"'
+        print(line)
+
+    print(f"Total size: {total_size}", file=sys.stderr)
+    print(f"Number of backrefs: {len(backrefs)}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
--- src/py/configurator.py
+++ src/py/configurator.py
@@ -26,6 +26,10 @@ import numpy as np
 import ctypes
 import argparse
 
+import lzma
+import base64
+import struct
+
 _libc_path = ctypes.util.find_library("c")
 _libc = ctypes.CDLL(_libc_path)
 
@@ -200,6 +204,17 @@ def get_screen_resolution(root):
     return screen_width, screen_height
 
 
+def parse_color_string(color_str: str) -> Tuple[int, int, int]:
+    if color_str.startswith("#"):
+        color_str = color_str[1:]
+
+    r = int(color_str[0:2], 16)
+    g = int(color_str[2:4], 16)
+    b = int(color_str[4:6], 16)
+
+    return r, g, b
+
+
 class Window:
     on_screen_area_updated = None
 
@@ -223,15 +238,19 @@ class Window:
 
     def __init__(self, root: tk.Tk):
         self.root = root
+        root_background_color = parse_color_string(root.cget("background"))
+
+        self.refs = []
 
         notebook = ttk.Notebook(root)
+        notebook.pack(fill=tk.BOTH, expand=True)
 
-        main_frame = tk.Frame(notebook)
-        main_frame.pack()
-        notebook.add(main_frame, text="Calibration")
+        calibration_main = tk.Frame(notebook)
+        calibration_main.pack()
+        notebook.add(calibration_main, text="Calibration")
 
         # Frame for IR canvas and buttons
-        calibration_frame = tk.Frame(main_frame)
+        calibration_frame = tk.Frame(calibration_main)
         calibration_frame.pack(fill=tk.BOTH, expand=True)
 
         # "IR canvas"
@@ -309,7 +328,46 @@ class Window:
         )
         self.bottom_entry.grid(row=3, column=1)
 
-        notebook.pack(fill=tk.BOTH, expand=True)
+        keybindings_main = tk.Frame(notebook)
+        keybindings_main.pack()
+        notebook.add(keybindings_main, text="Keybindings")
+
+        key_canvas = tk.Canvas(keybindings_main)
+        key_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10)
+
+        img = tk.PhotoImage(width=len(WIIMOTE_IMG[0]), height=len(WIIMOTE_IMG))
+        self.refs.append(img)
+
+        try:
+
+            def setalpha(x, y, alpha):
+                img.tk.call(img.name, "transparency", "set", x, y, alpha, "-alpha")
+
+            def to_hexcolor(r, g, b, a):
+                return f"#{r:02x}{g:02x}{b:02x}"
+
+            setalpha(0, 0, 0)
+        except tk.TclError:
+            # We are using a tk version that does not support setting linear alpha
+            # ... we accept this and use binary alpha
+            def setalpha(x, y, alpha):
+                img.tk.call(img.name, "transparency", "set", x, y, alpha <= 0)
+
+            def to_hexcolor(r, g, b, a):
+                blend_value = (
+                    sum(root_background_color) // len(root_background_color) * (255 - a)
+                )
+                r = (r * a + blend_value) // 255
+                g = (g * a + blend_value) // 255
+                b = (b * a + blend_value) // 255
+                return f"#{r:02x}{g:02x}{b:02x}"
+
+        for y, row in enumerate(WIIMOTE_IMG):
+            for x, pixel in enumerate(row):
+                img.put(to_hexcolor(*pixel), (x, y))
+                setalpha(x, y, pixel[3])
+
+        key_canvas.create_image(0, 0, anchor=tk.NW, image=img)
 
 
 class Logic:
@@ -2033,6 +2091,140 @@ KEYMAP = [
         "section_name": "Extended Keyboard",
     },
 ]
+
+
+def decompress_embedded_image(data):
+    rle_data = lzma.decompress(base64.b85decode(data.strip().encode("ascii")))
+    width, height, ncolors = struct.unpack("<HHH", rle_data[:6])
+
+    sample_unpack_str = "B" * ncolors
+    sample_size = struct.calcsize("<" + sample_unpack_str)
+
+    flat_image_data = []
+
+    backrefs = []
+    offset = 6
+    while offset < len(rle_data):
+        (reps,) = struct.unpack_from("<l", rle_data, offset)
+        offset += 4
+        if reps < 0:
+            reps = -reps
+            (ref_index,) = struct.unpack_from("<H", rle_data, offset)
+            offset += 2
+            sample = backrefs[ref_index]
+        else:
+            sample = list(struct.unpack_from("<" + sample_unpack_str, rle_data, offset))
+            offset += sample_size
+            backrefs.append(sample)
+        flat_image_data.extend([sample] * reps)
+
+    rows = []
+    for y in range(height):
+        row = flat_image_data[y * width : (y + 1) * width]
+        if len(row) == width:
+            rows.append(row)
+
+    return rows
+
+
+WIIMOTE_IMG = decompress_embedded_image(
+    r"{Wp48S^xk9=GL@E0stWa8~^|S5YJf5;D1XMd|d!IfCt+co!^P^Z_C>)-U6$C=!x8-%x>`Q77q"
+    r"z@MKwwi!;QE~psJQw0FOQT6c3pqA{?Fg9EW@#m}jqi$B?v}qJ**%5cQXM!!M10UFsc0_L^09?"
+    r"T5M;qC)5unLXoK=j&mWy+S4g)Jee?KWz@79j%X4WK3euEJdGcb0RZjPxY<)w5w;sN%@mj^CIK"
+    r"hIZ+h}OenkT8w+U90DCk(T1_uwoU&8}+Ls<)orxC;tAH1WY#*5%BABKF9~uV}XYl;4WA81x9i"
+    r"#G;(<AGusbpC(--p&H<o8Ou9LX}^!oQm>Py{EpLne7&OcBLlW13Xo<};pv^tZPlSx~i7vY~N9"
+    r"9e7<2BHo?r6Fv?$^$$eBA1XQX3_$5<i(5JK&!a`NL#;|5FtY**$sWUjSJR?*Sj)!;V<o*MZ}w"
+    r"EM9zaem`-ARoB4YlI3YmyC46phGBTQ&(oA2ZF0pGofk~J>RK`Re6q7p->(SPcc*k6)PEaR~h_"
+    r"<owR!WH)^;a?W2Ry;vDey=_~IKgBAd_wBA=l&&NqfY&vpQ`#OU3;;3Gp~YsM7X^1DDqs~{dKt"
+    r"&uE{_46)&1kjO0YDyRjms;3*LQuKU$avyp)3)Xl5Z^A7trFndz(K*`Ao>R)D~K>85ihKh~C0~"
+    r"2sg5w%hx&R@mf0)Vj_0i!81<9!wp#s*V%E;cGbA9VPbgd^&B;IxCAq^`X}a5YQWuHKCKozc?t"
+    r"pmlaWuZ4d}r^~qzgRUTgZ`!_LLN&n#jZMY_Pw&_Kh{J}FyQ4&JRf|B=letM<Cjo`)B6#N$$^a"
+    r"lra&e<#qwY8O87tW`z%20aF4^)4snV&B&~W4~9C;NghS}7Ngm(nOTGjRYj+1B!!{AzH6yYhQ^"
+    r"QSYdaDmj?5@R}jO>D$^4hvev)!c=_(suj!IhiqEjC!XqoKI??O11Ax>l&u#?iu_}KoN9>|B|y"
+    r"8IPI}fpUtHnyB+25w@4!3e?>4th%+jrByuUasx{|9?kfNm9><d`VGiL53A)iqy7KqRk##N4E7"
+    r"-_$S}cP3jXGj3m>^-_b=8_+Lw^*8UfZddXb)=WcZm_x-qa15ty4|GjxA*Fp1-!xp!8kgnhJAK"
+    r"x<-t_W&nBbE-LC+3yHa7@~xPQEng|QpRGXh>vi_aTwJPujbp7(ZH`FIid=Pe;Thm;Y`osO{vm"
+    r"3(ZEQB+!KqFt{OG+x=aE8UrIfE1pwJe+!HS%(jda7RUggc4e%rmd0WwM;*<{CwV#<Q%-T-Of7"
+    r"WwUhiy;gzezE;<W2!#1(xjU4)v}HMDO<<lA>FSB{D|H9(-amLUriUfWwftqJB*F2^add@OL7O"
+    r"8a)P@X#uMgV5dF3khq)+Sad|>DsittL^Ik3o+X8yI(-O92$+2%`S1BK%Rh-~>zJ;py;0#GSL*"
+    r"fyh?t^bH3<K7hNDx^9zY*wYefLgt3Z+AFaLU|2q6Cu1o`;0=#!sazPzn}cgSC}+@~=hh{W70B"
+    r"dm;x;XPcq?;}?`sG%D1Iz2Dvwl$YPI^(<pZP=qvmR0LXvQjJ2yze6wd0j|HhuM}6~+OY?LknJ"
+    r"`7?Gld+3jVXC72a?2B=v0)=ZqB^b|n)0X&D|6J0NjI>Ey$~(#H_8o4(j@V`RlfMzWB_qxsF^e"
+    r"*=z+bbG2o75)7&{Z6unuwikZ-E@_AzP}1)EriP9uynzLuS7mc<Y}WMeZVDf6MU}!_Tfju?}gC"
+    r"bylqAPW*v)j#QQ-~O=lkT#-9jke8fLYpscqXey&(>mYrD`dD9X55ymF|ex8`@^xR`XP9JqVow"
+    r"_xPk9w8M9iSd`!r!gUA**gJZ?9Mos@k8QPb=Ng5Ln7^2~^xm2LuAUhKsn)R{7_29Pr1fAK0u^"
+    r">ar_Qm3`q2Ad!uMdEVrL!#byi3Vr*4b9f=_?30a=SV>7xOof6q%?chhO?%T!0;?#K>e?h|*=n"
+    r">_KE9p%24CTxt~9WHA}SFbqho&MgTBaT$S=Bfmwr3o1J%7PI+=rslg7LaFp#la@3nu3`~qE-8"
+    r"`zo?d$jj6gA)_&EOTI}s}&Gj{=$Wn4GM(?jmR(ak<lbjkHBTFmV#A-f>m?4h#a^lqPl1(dX@8"
+    r"%JK{tpn^B3KCFtNMhz`{CP^C-)*<tX>`aF@^5-d9cGKnKclh~i*JcqQ_Qd@E`VFoVT1P=>An0"
+    r"AE&Sp5Ry0q0lLo<>yANMi{bp9>gg45FQ}P2aut$`Bsiqp2;cCxMr=j7o&&P=b?}YUyvx;L4yi"
+    r"5^@n8j7w(1=K&$2Fc}_fZ}j#<W)nt0cI(s)d<B3DCi2$k{!_epFyZ_774CVs#86h^#4gQiY#T"
+    r"3RYPV1gmc)X^cT{|xGe8{^iR$q6veWfG>};&#4~jKHp=Z=torh2nCT7Vp5-z2!NJ{F>;}XR-%"
+    r"jZSLU!1FBLglyzqhi+IQHk~m6c%C)Huo6#p4ExG+&cwfj;PkYeaxXp$6FVFKL|AI>G0GTi{-}"
+    r"C17%CslnCgEmBmfF_r#FT81d~Kz>9a7=4Caa*P$xq(tJ<xO?z?8VBWl$AuV%++rL@HUMG3W^Z"
+    r"MfuqDawkXsdg`A$!hEnSEKq0vh^2b7Z7Jvkv|(2+4`75IQ*1wwOy}B^SBR`%|Kkk6bVV#+-`S"
+    r"a9_+pTS05xOS_WP&d@7I|F$ZWuB7#g8;)e&%STl!%z0psX|&o)^95TC+F#^DW6nM88QdRw35-"
+    r"T0l-{0B!w`_~T?}=ZYo}M#O52QeOCJScjNn?2J{JUFk=4}%ny{9P)}VB!`T-K3I`f%5%5nsA>"
+    r"CLckGR+*%sUy&Jcu+W^NCdCh?-*k6t$Vh{68X<`jEMF<gA4_c@-LbfvCSS$;7Fb_v9@>))4>@"
+    r"h?4Vwvxm;jtyQrl%npcmGTYzJ`G+bZ<jnC>~?z1DdB~wwUbmuM+nri>Y4sG<#sx3!QpnFHl*|"
+    r"?G${7B-)qA7lqVpUK%BNzT+9HJzS^<-m$kUuBA+g=|xY8#J<w3aSh{MUPfXHZt)XuXuwS-KWw"
+    r"@Hkv#|Jw3X?6Uk25@m%zL%)kGx!=aW6UM+Kd9RfQPbBza2nrYoVJO7I?P)$yoh{0ROhLU$iqy"
+    r"S`8s;5EZQ1>gA-J8$@1$I2ZeZ8VLUBkMc8>8K@*|tCvQJSQz_|&2YT|VAJ)<md`U_Mqh8dT{#"
+    r"E!x)jm8A^PiN1J<uf{fLN=bmvH!Ee7@zL#S8=?eC?S(jo)9Qu$SNz=xFcW3MK8|f@jWd82)WZ"
+    r"i4-cCobXeN92iCFCIXxJY3<2a@O7>^$oMmlV5DzupErTi_UV>xB2pY0_DDU`ilUe)Et{r*jBr"
+    r"@O!w=YlcvaIVDX~SEUm6KB@k>eM?RH#Y^oY%yhhnIROF4x${QL6f)@E{{S%t+(+JmW4k^PE>>"
+    r"n|?#eXG*mO>?7H@c9m=L*Uu^SfOJkRl#tByWa__PaX?C>_m<^>PGA2Xr>K+pEOZdxLyWc1Fom"
+    r"*nNX$uUQ(4DmCdG6w!L50VcgEI3OmC&Y{n)Ulfq`xW)^0H)UtLd>JgP+PMU&5SY8Lhr`92rmB"
+    r"RP?{_QYt2^!VG1ikBkBOhpkBlRc5By6jW?EI{+nm6>Aa_f0jnNGjY#rEQ3QW&nwf`t7#=to!#"
+    r"@cWTDv((>?fa7hW()rsc8`Wrs2jCF_ga$Ro9fwhICO@UoCQ2Vc+r-G@>hJfZQjmDI}JJA{l6f"
+    r"vUw52&4iMqp>79vM2n{Qa^Nd(|G?kXmpzU&3sgDO$py3VJkVwAVg3YHn0Yap{vrxhWC%oRXk}"
+    r"p`@jWmKWoBAALVuSH&GXYio4Z$sH^2GY1zCx9+(VLA}!#N7&;FK_#^5(wdRXfGY0xoyJ|E!x|"
+    r"Hc(XMq#9<U{UOZ{4aK;PHid`ICCZnI-)b=i|@)-l~8SOWfnrU5d(#J%IE_3AbOgz(rk;a^Y<U"
+    r"bv%Ay)Ml&dH@N*q5mzR+GtV!KfHW`C3UJ3Jw<C&n?Ub`kJF<UG~R62Xb|y(#*d3UD*M;s>CWl"
+    r"~V|mK={&3{z1>lzCmQ<RoM&N{7E3fFDv*bpJtnMhoLw0yjYY60ju;=yLjV76E%$wlMQ)oHf1E"
+    r"-kTR6z^5o;hZe{*FM&y6C`PsGcX1Me~SmsdY}l@eKb&j@rkE658U{X<AlgRWz8mjMMGke8}6F"
+    r"LJtPS9K!9(T{4xtCN0sJ;pl@?68!03a7BrzaXxBDUCj-$t=3>EQR2>^^Py^PT|#)J(k1n30+y"
+    r"(Ag6*p67$3Okx5#!`j2o6sV=AyPR5G-l6`0v6=z;PfPro(!rth708mLuQh*7^=Nyi=6jnhgp)"
+    r"FglU3k>%VxeLa#9`TikYoO_z8On*xEjpF5kok<1|G(Y3qE;F+3zJJC5@bRDrhkTv<Ov$VE<$V"
+    r"aJLQxeqUIgkWk$gb+7(8nS^H2~sh*b#)H4{rUotYQ`Hrf5(aLqck2%FPm1q)Ryf>u&oYe+p!7"
+    r"a8NbWw6m0@s<tp^$!|A+3Jvd}((m0Y=#1ZjvU<*3Z_aNS=W?uOvkf-sSF}V<edNSa209z{6k%"
+    r"SC?j*;z~$FgM}&SPx(BFEWMue*C)H6C%B?i{r1#6oCt=8Ow?$$6`2UeX3&Vr7!M6|X=QmOevU"
+    r"!o9;V28bVJc&I@b8!=v1TM=(B<}5%9d~lQLq?MO-l(LA8m(6>^N`Nw%j1CfTc?4i-opoV$dad"
+    r"JB-#q1AjGr9PDe9IiQe;agi;^as@qwH|IhTZhsCXroqx^Wf~!mj1`0|LagbiEiDZPd~x!T!qf"
+    r"ZXI4jhr~hH8nj#iX4scr+C1Dnap%=Q<uZnrY-o*nyw&lj+s#)>*SSx3=c;ag*No;3S6LvJB|F"
+    r"bZ)J*?zS<B+>cBgz3132giK#<HSD^<UT#7xj`EMxX+wucg{8mou>7BF$c7jmitIcTs#NZ)$f8"
+    r"5CY5jZ0jINEqBqM$(@js1CuLK9il;aXIogVFxit^xS9q{{>YHt2*yBie@Qr<_6B36)_s!m+l>"
+    r"S>nX71VX1qaPKYFl*3O;xH^jARM)P7W9xb3Y4kmLT+eS6zHg+2F=zX(&*JdQwvbZy(*YS48@A"
+    r"i936Xdxd56|XP<^;m_%>qnINetJy7c=w57t0{9!-D+&{8YPXwT70RU81t5iv%kkUBlpr>2f&h"
+    r";3Yi^U;a#oYSq}aQ+N3lp_8wl9wSf&ts3}g!S0u;dfa&GRe~GCoU35F(TQxh3J<w3q959*rwv"
+    r"^G$*iRCn4>U6o)(CWu=`p{T)7@COn_W?3ERzzTJ?73AZug-hWsB$nAldhW01s`!brl9f%{1`n"
+    r";zdyJHsVv@R_%BDp+^C76e!!dWgYq$fM~5BhRJ<OFQwDNeWgGdj~hRdz5tI&bI<cD0~T}kN|C"
+    r"BnS+_^}|4Z-|vfwVk3zE~Y$?hf6@6`9#07M)9qJn`?1EnKyT}?LEd6Su8m~>tkI~jT^%C~9W?"
+    r"2`%XnTacuAS;+~VHQSiYn!wWWUw&$+SccKgqtCZYpyXN1Y`uvlfBucBS56KskKqjgO$LNNAM7"
+    r"xF%bH22Hg{bIqSTsdLWib$nA2J5pVmM$y(Q)mQANRY={Z`_7i&MiIsTOM2{CL>vHU(=<f?hzY"
+    r"bzOsnN#?<M&Ti4H}dh?qE1-knsU<+axOW2T}Rd=NBO15)|u0`<+5X*Jg$yeaOnU_tcG8?`|Wg"
+    r";U(W-B2YDPWDEt0R$tFHRh~~izwiG~Pn#Lh?u;&GJ;hA&zv_<ljG+(c1o(2SNmxdv>)kj5I>*"
+    r"oKDh8xAn#}wB<G$<55-p~Ugj`LiLz+NV`CKqikwK(6VUZ5TU&+FEOxS(S1?l`pyqj7IMZ`2pn"
+    r")mW`S4%YQYv`5<hZVulFKbS5>eS$Hu-60e-FaM`m2e8T<W9*_CDT##EPqW(C=I|_?|#trHT;+"
+    r"EqSZbJ9J-nMhM4J9f+wta<k)8V{Og_aBh9a5T8Mw69FeN|t(%-lxc_1WK29{NLO16xb0HJcoB"
+    r"l@Ovx3QWHurzE8M6@p`LxaC7a6~}(v>-FKY%**Qn{{cUrT{#dclIgx%<>2O`>8`J6STZgCHhp"
+    r"ygb2`s&QXcoW~^D$c`aIFdU5n=To^YgDOr~S>-!sZ-1TsxjRMJyR7MflAuA4V_%2>q{fLD5Xx"
+    r"5V8I-hXe}29e);O6!*wlh!!_u@EAqXE_DFnP!bYK8~TJs|GX)ukZ18V>6H3Q&0Q;{v7OyXsrC"
+    r"wb<26BOBd2awB^-|RyF12(mTc(+}J^)157>0JE{0fbm?hmDfHuBO|w1X*^)fk_$tZ80bj<!}R"
+    r"Ypbnmj0+f?N)cptUP@v&|7mk#+IzGjZWUeonqG1YCw1tD|`)ZNp1uLBK%f9U%icrY7AM=tiu@"
+    r"WK9ZvPkdZ6j2c0>W4mm4mEoR?%O>?5nK0uAryv77r+~@N*ei`v@RCXJezt+>p_!4b)Ts=dcaQ"
+    r"QH(`xUhn-~3V}`U4g!VEa$Il3Y7L5bOOl51b*Ojf3rP}I3NH`R&20GqZkk*1=;`*6X1P>lSBz"
+    r"85+e;1VHofh%xZj@KGg9Y>&IlLlEoyAc%2Mj|^qLvC(b+BvWos?FNGg$Z0}GIY0>I`klu>{oQ"
+    r"d}R)Yv~!TBVM;~Dj2p~MF+p9#BLpz(NUyn7y|;;dj4y}k~;h|WkuT`{I*paFhRy9Dd7{OfUCi"
+    r"$*jSPDtnb^pwUeD*mm82gAW{&zZF$J-lE9j*fNubFB1xcO9|R$*$8gocnp!HeEk#$4{~!MhD~"
+    r"G@*9d67SgG<&xj@9SW5Tfs`u87N$+jlO;eg9fiM<TQT5$+h$lKz*N5>i8#SN8@sfq8(2!p24H"
+    r"G}n`QW2yJ$-Cj@+SsQ-x`IX!gyHfdyql;z09dq_iRh7~@Phn$T<OtK);|Y|ju{21v4+flUy-l"
+    r"ueg3bo)93~l^|9?iIxkbdsuPRbjruIyR5HcPO90+DhP->yW58HlcVY`MUZ5)Tb$Bbr353jjMr"
+    r"hmrZc(678$%Fu&Gn(1+_}n%z9+^l$JF5;PBP@p>89@hUPX;6-%GC`Vj(KpSV$^K$J9*2zNG~7"
+    r"IyfP_9!^kzlzhI!`m{Mn62tq!&0U&3f_c7C@ZHQ!jh?@`cyC_sG9$}MXOS$UIHxf6Z^S3;1tW"
+    r">+E(A>bP<eR2vfDP@8TCXA+o86cwykXTgCVcyuRKyuHQKr-otCa!(2(eie^gisE00EdQ%>DrY"
+    r"!(gi(vBYQl0ssI200dcD"
+)
 
 if __name__ == "__main__":
     main()
